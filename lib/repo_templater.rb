e88ddcb0c6128b5ca3ed237397ca8f64ce0f9d2e
Igor Padoim <igorpadoim@gmail.com>
2025-10-08 15:00:44 -0300

    Document current entities

--- lib/repo_templater.rb
+++ lib/repo_templater.rb
@@ -3,8 +3,11 @@
 require 'pastel'
 require_relative 'repo_templater/meta'
 
+# Main module
 module RepoTemplater
+  # Actions for the CLI
   class Actions
+    # Display info about the gem
     def self.info
       pastel = Pastel.new
       puts "#{pastel.cyan(RepoTemplater::NAME)} by #{pastel.blue(RepoTemplater::AUTHOR)}"
@@ -13,6 +16,7 @@ module RepoTemplater
       puts "Available under the #{pastel.green(RepoTemplater::LICENSE)}."
     end
 
+    # Run the main functionality
     def self.run
       # TODO: implement main functionality
     end
--- lib/repo_templater/meta.rb
+++ lib/repo_templater/meta.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 module RepoTemplater
+  # Gem name
   NAME         = 'Repository Templater'
+  # Gem slug
   SLUG         = 'repo_templater'
+  # Gem description
   DESCRIPTION  = 'Create a series of metafiles to begin a new project, like README, LICENSE, .gitignore and more.'
+  # Gem version
   VERSION      = '0.1.0'
+  # Gem author name
   AUTHOR       = 'Igor Padoim'
+  # Gem author email
   AUTHOR_EMAIL = 'igorpadoim@gmail.com'
+  # Gem author website
   AUTHOR_URI   = 'https://nereare.com/'
+  # Gem license
   LICENSE      = 'WTFPL'
+  # Gem copyright year
   YEAR         = '2025'
 end
