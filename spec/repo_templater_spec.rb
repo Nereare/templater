1e5d84b474fa2772df37f1dee1d4b6762a4ee7aa
Igor Padoim <igorpadoim@gmail.com>
2025-10-11 14:46:00 -0300

    Test for gem constants

--- spec/repo_templater_spec.rb
+++ spec/repo_templater_spec.rb
@@ -1,11 +1,39 @@
 # frozen_string_literal: true
 
 RSpec.describe RepoTemplater do
-  it 'has a version number' do
+  it 'has a name' do
+    expect(RepoTemplater::NAME).not_to be_nil
+  end
+
+  it 'has a slug' do
+    expect(RepoTemplater::SLUG).not_to be_nil
+  end
+
+  it 'has a description' do
+    expect(RepoTemplater::DESCRIPTION).not_to be_nil
+  end
+
+  it 'has a version' do
     expect(RepoTemplater::VERSION).not_to be_nil
   end
 
-  it 'does something useful' do
-    expect(false).to be(true)
+  it 'has an author' do
+    expect(RepoTemplater::AUTHOR).not_to be_nil
+  end
+
+  it 'has an author email' do
+    expect(RepoTemplater::AUTHOR_EMAIL).not_to be_nil
+  end
+
+  it 'has a author website' do
+    expect(RepoTemplater::AUTHOR_URI).not_to be_nil
+  end
+
+  it 'has a license' do
+    expect(RepoTemplater::LICENSE).not_to be_nil
+  end
+
+  it 'has a copyright year' do
+    expect(RepoTemplater::YEAR).not_to be_nil
   end
 end
